e0c1b596d84509d8af1ac22695dd3f1f37be8187
sac-iiitnr <115001496+sac-iiitnr@users.noreply.github.com>
2025-12-11 08:02:24 +0530

    changed form link

--- src/Components/SubmitIdea.jsx
+++ src/Components/SubmitIdea.jsx
@@ -4,7 +4,7 @@ export const SubmitIdea = () =>{
     return (
         <div className="">
             <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSd_XP-cGs7wKU9QJ9VkP-S8GaaSDEk-TfzqJ5og2VsDLlJ9jA/viewform?usp=sharing"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSf4fsyPWqmiVY5thgJNao-a8xZRELwj7GjCSKjR2HYOmjh_kg/viewform"
               target="_blank"
               class="text-[#5172E7] w-[250px] h-[48px] rounded-full border drop-shadow-lg flex justify-center items-center bg-white font-semibold text-opacity-80 cursor-pointer transition-transform transform hover:scale-105 hover:drop-shadow-xl duration-300 ease-in-out "
               rel="noreferrer"
